7ef7a83ae9d5d48346dfeb0d9cc40f47f6075944
DimitrisVlachos <DimitrisV22@gmail.com>
2013-09-09 06:50:30 -0400

    Update file_stream.hpp

--- file_stream.hpp
+++ file_stream.hpp
@@ -48,11 +48,7 @@ namespace file_streams {
 		bool is_open() { return m_fp.is_open(); }
 		const char* identity() const { return "file_stream_reader_c"; }
 		bool eof() { return m_fp_size == m_fp_offs; }
-
-		bool close() {
-			m_fp.close();
-			return true;
-		}
+		bool close() { m_fp.close(); return true; }
 
 		bool open(const char* path) {
 			std::streampos tmp,tmp2;
@@ -87,19 +83,19 @@ namespace file_streams {
 				return 0;
 
 			tmp = ((m_fp_offs + len) > m_fp_size) ? m_fp_size - m_fp_offs : len;
+			if (0U == tmp)
+				return 0U;
 			m_fp.read(static_cast<char*>(dest),tmp);
 			m_fp_offs += tmp;
 			return tmp;
 		}
 
 		uint8_t read() { 
-
 			if (eof())
 				return 0;
 
 			++m_fp_offs;
 			return m_fp.get();
-
 		}
 	};
 
@@ -114,21 +110,11 @@ namespace file_streams {
 		~file_stream_writer_c() {  this->close(); }
 
 		const char* identity() const { return "file_stream_writer_c"; }
-	 
-		uint64_t tell() { return m_fp.tellp();  }
-		uint64_t seek(const uint64_t offs) { 
-			m_fp.seekp(offs,std::ios::beg);
-			return m_fp.tellp(); 
-		}
-
-		bool is_open() {
-			return m_fp.is_open();
-		}
-
-		bool close() {
-			m_fp.close();
-			return true;
-		}
+		uint64_t tell() { return m_fp.tellp(); }
+		uint64_t seek(const uint64_t offs) { m_fp.seekp(offs,std::ios::beg); return m_fp.tellp();  }
+		bool is_open() { return m_fp.is_open(); }
+		bool close() { m_fp.close(); return true; }
+		bool write(const uint8_t v) { m_fp.put(v); return true; }
 
 		bool open(const char* path) {
 			uint64_t tmp;
@@ -140,16 +126,82 @@ namespace file_streams {
 		}
 	 
 		uint64_t write(const void* src,const uint64_t len) {
+			uint64_t offs;
+			if (0U == len)
+				return 0U;
+			offs = tell();
 			m_fp.write(static_cast<const char*>(src),len);
-			return len;
+			return tell() - offs;
+		}
+	};
+
+	/*
+		RAM streams
+	*/
+	class file_mem_reader_c : public file_stream_if {
+		private:
+		uint8_t* m_ram;
+		uint64_t m_fp_size,m_fp_offs;
+		bool m_cleanup;
+
+		public:
+
+		file_mem_reader_c() : m_ram(0),m_fp_size(0U),m_fp_offs(0U),m_cleanup(false) {}
+		file_mem_reader_c(uint8_t* mem,uint64_t len,bool cleanup_shared_mem) : m_ram(mem) ,
+	 	m_fp_size(len),m_fp_offs(0U),m_cleanup(cleanup_shared_mem) 			{   }
+		~file_mem_reader_c() { this->close(); }
+
+		uint64_t tell() { return m_fp_offs; }
+		uint64_t size() { return m_fp_size; }
+		bool is_open() { return true; }
+		const char* identity() const { return "file_mem_reader_c"; }
+		bool eof() { return m_fp_size == m_fp_offs; }
+		uint8_t read() {uint8_t t; if (eof()) return 0; t = m_ram[m_fp_offs++]; return t; }
+
+		bool close() {
+			if (m_cleanup)
+				delete[] m_ram;
+
+			m_ram = 0; 
+			m_fp_size = 0U;
+			m_fp_offs = 0U;
+			m_cleanup = false;
+			return true;
 		}
 
-		bool write(const uint8_t v) { 
-			m_fp.put(v);
-		 	 
+		bool open(uint8_t* mem,uint64_t len,bool cleanup_shared_mem) {
+			close();
+ 			if ((!mem) || (!len))
+				return false;
+			m_ram = mem; 
+			m_fp_size = len;
+			m_fp_offs = 0U;
+			m_cleanup = cleanup_shared_mem;
 			return true;
+		}
+
+		uint64_t seek(const uint64_t offs) { 
+			if (offs > m_fp_size) {
+				m_fp_offs = m_fp_size;
+				return m_fp_size;
+			}
+			m_fp_offs = offs;
+			return m_fp_offs; 
+		}
+
+		uint64_t read(void* dest,const uint64_t len) {
+			uint64_t tmp;
+
+			if (eof())
+				return 0;
+
+			tmp = ((m_fp_offs + len) > m_fp_size) ? m_fp_size - m_fp_offs : len;
+			if (0U == tmp)
+				return 0U;
+			memcpy(dest,(const void*)(m_ram + m_fp_offs),tmp);
+			m_fp_offs += tmp;
+			return tmp;
 		}
 	};
 }
 #endif
-
